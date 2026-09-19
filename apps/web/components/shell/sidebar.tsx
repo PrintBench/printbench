fd02cec2f544222aa59c0f30b5cfa89efce7ae22
Harold Roberts <hroberts65616@gmail.com>
2026-09-19 14:02:01 -0500

    fix: keep account menu visible in sidebar

--- apps/web/components/shell/sidebar.tsx
+++ apps/web/components/shell/sidebar.tsx
@@ -59,7 +59,7 @@ export function Sidebar({
 
       <aside
         className={cn(
-          'fixed inset-y-0 left-0 z-50 flex w-64 shrink-0 flex-col border-r border-[var(--color-border)] bg-[var(--color-surface)] transition-transform lg:static lg:translate-x-0',
+          'fixed inset-y-0 left-0 z-50 flex w-64 shrink-0 flex-col border-r border-[var(--color-border)] bg-[var(--color-surface)] transition-transform lg:sticky lg:top-0 lg:h-dvh lg:translate-x-0',
           open ? 'translate-x-0' : '-translate-x-full',
         )}
       >
